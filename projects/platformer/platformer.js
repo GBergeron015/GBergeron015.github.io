826aa6d116c808b12f5d5ab0932d4af28c4e30ce
GBergeron015 <gb8219769@lpssonline.com>
2023-09-12 15:12:09 +0000

    platform game added

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,17 +43,25 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
+    createPlatform(500,615,100,15)
+    createPlatform(825,565,100,15)
+    createPlatform(975,435,25,15)
+    createPlatform(635,320,100,15)
+    createPlatform(305,260,100,15)
+    createPlatform(0,200,50,250)
+    createPlatform(1100,360,100,300)
+    createPlatform(1200,610,50,50)
+    createPlatform(1350,500,50,50)
+    createPlatform(1200,400,50,50)
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable('diamond',0,100,0.5,1)
+    createCollectable('database',305,200,1,1)
+    createCollectable('steve',1100,250,1)
 
 
     // TODO 3
@@ -61,8 +69,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
+    createCannon("right",615,1500,50,50)
+    createCannon("bottom",150,1000,50,50)
+    createCannon("left",250,1500,50,50)
 
 
     /////////////////////////////////////////////////
