a572f5d63b89afd8ab9f99fb3986974a6f6ac6f2
GBergeron015 <gb8219769@lpssonline.com>
2024-05-09 14:58:01 +0000

    saving image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,9 +20,9 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
-  
-
+  applyFilter(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
   // do not change the below line of code
   render($("#display"), image);
 }
@@ -32,18 +32,54 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
-
+function applyFilter(filterFunction){
+  for (var i = 0; i < image.length; i++){
+    var temp = image[i];
+    for (var j = 0; j < temp.length; j++){
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
-
+function applyFilterNoBackground(filterFunction){
+  var i = 0;
+  var bgclr = image[i][i];
+  for (i; i < image.length; i++){
+    var temp = image[i];
+    for (var j = 0; j < temp.length; j++){
+      if (bgclr != image[i][j]){
+        var rgbString = image[i][j];
+        var rgbNumbers = rgbStringToArray(rgbString);
+        filterFunction(rgbNumbers);
+        rgbString = rgbArrayToString(rgbNumbers);
+        image[i][j] = rgbString;
+      }
+    }
+  }
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(bind){
+  var tempo = Math.max(bind, 0);
+  tempo = Math.min(bind, 255);
+  return tempo;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(reads){
+  reads[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
-
-
+function decreaseBlue(bleu){
+  bleu[BLUE] = keepInBounds(bleu[BLUE] - 50);
+}
+function increaseGreenByBlue(gtb){
+  gtb[GREEN] = keepInBounds(gtb[BLUE] + gtb[GREEN]);
+}
 // CHALLENGE code goes below here
